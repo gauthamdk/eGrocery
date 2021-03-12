277b6feaf6fb53d3a026278dcfae5dec99d74633
Gautham <gauthamkumar.gk.gk@gmail.com>
2021-03-12 23:36:34 +0400

    added routes

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ let express = require('express');
 let app = express();
 app.set('view engine', 'ejs');
 
+//routes required
+let aboutus = require('./routes/AboutUs');
+let login = require('./routes/LogIn');
+let register = require('./routes/Register');
+let catalog = require('./routes/Catalog');
+let cart = require('./routes/Cart');
+let profile = require('./routes/Profile.js');
+
+
 let port = process.env.PORT || 3000;
 
 app.get("/", (req,res)=>{
     res.render("home")
 })
 
+app.use('/about', aboutus);
+app.use('/login', login);
+app.use('/register', register);
+app.use('/catalog', catalog);
+app.use('/cart', cart);
+app.use('/profile', profile);
+
 app.listen(port, ()=>{
-    console.log("App running on port" + port);
+    console.log(`App running on port ${port}`);
 })

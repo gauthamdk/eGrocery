52a3cae0e807f0dd21d28838a0e8078afdca40c8
Pedro <pedro209.vd@gmail.com>
2021-03-12 18:39:17 +0400

    Delete styles.js

--- Pallete/styles.js
+++
@@ -1,9 +0,0 @@
-const divs = document.querySelectorAll("div");
-
-for(let div of divs) {
-	color = window.getComputedStyle(div).color;
-	// console.log(color)
-	div.innerText = color;
-	div.style.color = "black";
-	div.style.background = color
-}

f66308d35f95c9e4b11ec2d307c042fd4c371816
shaibu <shaibusunnuma@gmail.com>
2021-03-23 02:04:22 +0000

    Took out comments

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(passport.session());
 
 /* MONGOOSE SETUP */
 
-/*const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 mongoose.connect('mongodb://localhost/userAuth',
@@ -37,11 +37,11 @@ const UserDetail = new Schema({
 });
 
 UserDetail.plugin(passportLocalMongoose);
-const UserDetails = mongoose.model('userInfo', UserDetail);*/
+const UserDetails = mongoose.model('userInfo', UserDetail);
 
 /* PASSPORT LOCAL AUTHENTICATION */
 
-/*passport.use(UserDetails.createStrategy());
+passport.use(UserDetails.createStrategy());
 
 passport.serializeUser(UserDetails.serializeUser());
 passport.deserializeUser(UserDetails.deserializeUser());

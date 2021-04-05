8ce8a7e7f196a9d84b6186de1fa04645502d0f42
Gautham <gauthamkumar.gk.gk@gmail.com>
2021-04-05 21:45:00 +0400

    added route for aboutus

--- routes/AboutUs.js
+++ routes/AboutUs.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', (req,res)=>{
-    res.send("About Us Page");
+    res.render('about');
 })
 
 module.exports = router;

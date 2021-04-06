a9fc1b1355eb39c2c9533324f4e0cdf98dea2c06
Gautham <gauthamkumar.gk.gk@gmail.com>
2021-04-07 01:52:31 +0400

    removed login to access catalog

--- routes/Catalog.js
+++ routes/Catalog.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 const connectEnsureLogin = require("connect-ensure-login");
 
-router.get('/', connectEnsureLogin.ensureLoggedIn(),
+router.get('/', 
   (req, res) => {
         res.render('catalog');
     })

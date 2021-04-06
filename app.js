a250405b78d116f7dc5ab07255321c787fc419ae
Gautham <gauthamkumar.gk.gk@gmail.com>
2021-04-07 02:39:05 +0400

    fixed catalog view

--- app.js
+++ app.js
@@ -57,11 +57,11 @@ passport.use(UserDetails.createStrategy());
 passport.serializeUser(UserDetails.serializeUser());
 passport.deserializeUser(UserDetails.deserializeUser());
 
-data = {    name: 'Apple',
+data = {    name: 'Orange',
   details: "lorum ipsum",
-  stock: 10,
+  stock: 5,
   category: 'Fruits & Vegetables',
-  image_url: 'https://images.unsplash.com/photo-1600626333392-59a20e646d97?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+  image_url: 'https://images.unsplash.com/photo-1613747495731-3add0988073a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
 }
 
 // Products.create(data, (err, product) =>{

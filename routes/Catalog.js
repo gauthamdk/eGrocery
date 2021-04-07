46e6a5bad0a90b19ca5a3ffd0d72d3e12e453263
Gautham <gauthamkumar.gk.gk@gmail.com>
2021-04-08 02:52:54 +0400

    design change and filter by category

--- routes/Catalog.js
+++ routes/Catalog.js
@@ -1,19 +1,28 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let Products = require('../models/Product');
+let Products = require("../models/Product");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+	Products.find({}, (err, allProducts) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("catalog", { products: allProducts });
+		}
+	});
+});
 
-  Products.find({}, (err, allProducts) =>{
-    if (err){
-      console.log(err)
-    }
-
-    else{
-      res.render('catalog', {products: allProducts})
-    }
-  })
-}
-)
+router.get("/:category", (req, res) => {
+	Products.find(
+		{ category: req.params.category },
+		(err, filteredProducts) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.render("catalog", { products: filteredProducts });
+			}
+		}
+	);
+});
 
 module.exports = router;

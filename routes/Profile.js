e4fa153447d6545ecc748920829c3f23a2729e5f
Gautham <gauthamkumar.gk.gk@gmail.com>
2021-04-26 12:25:31 +0400

    fixed profile backend

--- routes/Profile.js
+++ routes/Profile.js
@@ -5,45 +5,33 @@ const User = require("../models/User");
 const Product = require("../models/Product");
 const passport = require("passport");
 let ObjectID = require("mongodb").ObjectID;
+const { update } = require("../models/User");
 
-// router.get("/", (req, res) => {
-//   res.render("userprofile");
-// });
-
-router.get("/:userid", connectEnsureLogin.ensureLoggedIn(),(req, res) => {
+router.get("/:userid", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
   User.findById(req.params.userid, async (err, user) => {
-    // const name = user.name;
-    // const email = user.email;
-    // const phone_no = user.phone_no;
-    // const address = user.address;
-    // const payment_method = user.payment_method;
-    res.render("userprofile", {user: user});
+    res.render("userprofile", { user: user });
   });
 });
 
 router.put("/:userid", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+  console.log(req.body);
   const newdetails = {
     name: req.body.name,
     email: req.body.email,
-    phone_no: req.body.phone_no,
+    phone_no: req.body.phone,
     address: req.body.address,
-    payment_method: req.body.payment_method,
+    payment_method: req.body.payment,
   };
 
-  User.findByIdAndUpdate(
-    req.params.userid,
-    newdetails,
-    (err, updatedUser) => {
-      if (err) {
-        console.log(err);
-      } else {
-        //req.flash("success", "Saved details");
-        console.log("Saved details");
-
-      }
+  User.findByIdAndUpdate(req.params.userid, newdetails, (err, updatedUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      req.flash("success", "Saved details");
+      console.log("Saved details");
+      res.redirect("/profile/" + updatedUser._id);
     }
-  );
+  });
 });
-
 
 module.exports = router;
